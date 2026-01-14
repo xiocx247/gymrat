a4e5177174d656f33bb6f5ed2fa6248662c54025
Xio <cx2402x@gmail.com>
2026-01-13 22:03:19 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+print ("1. Log today's gym visit")
+print ("2. View workout history")
+print ("3. Exit")
+
+choice = input("Enter your choice: ")
+if choice == "1":
+  print("You chose option 1!")
+elif choice == "2":
+    print("You chose option 2!")
+elif choice == "3":
+    print("You chose option 2!")
+else:
+  print("invalid option")
+
